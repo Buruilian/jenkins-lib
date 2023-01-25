4d4be70ee152c7366b3bbcbed614a878624aa756
Administrator <2633018506@qq.com>
2023-01-25 17:17:22 +0000

    Update ci.jenkinsfile, cd.jenkinsfile, src/org/devops/Gitlab.groovy

--- src/org/devops/Gitlab.groovy
+++ src/org/devops/Gitlab.groovy
@@ -12,13 +12,13 @@ def GetCommitID(){
 // namespace 
 // usera/devops-service-app
 // userb/devops-service-app 
+
 def GetProjectID(projectName, groupName){
     response = sh  returnStdout: true, 
         script: """ 
             curl --location --request GET \
             http://172.31.1.10/api/v4/projects?search=${projectName} \
-            --header 'PRIVATE-TOKEN: N-kxsLC3KMZkfX8NJiLR' \
-            --header 'Authorization: Basic cm9vdDoxMjM0NTY3OA=='
+            --header 'PRIVATE-TOKEN: j53sXHNtGH3s7GGsT-CD' \
         """
     response = readJSON text: response
     if (response != []){
@@ -28,4 +28,22 @@ def GetProjectID(projectName, groupName){
             }
         }
     }
+}
+
+def HttpReq(method, apiUrl){
+    response = sh  returnStdout: true, 
+    script: """ 
+        curl --location --request ${method} \
+        http://172.31.1.10/api/v4/${apiUrl} \
+        --header 'PRIVATE-TOKEN: j53sXHNtGH3s7GGsT-CD' \
+    """
+    response = readJSON text: response - "\n"
+    return response
+}
+
+
+def GetBranchCommitID(projectID, branchName){
+    apiUrl = "projects/${projectID}/repository/branches/${branchName}"
+    response = HttpReq("GET", apiUrl)
+    return response.commit.short_id
 }
