fd800eaee61a2f595978ecf10bcdafb332fb9a64
Administrator <2633018506@qq.com>
2023-01-27 23:09:51 +0000

    Update src/org/devops/Gitlab.groovy

--- src/org/devops/Gitlab.groovy
+++ src/org/devops/Gitlab.groovy
@@ -1,5 +1,29 @@
 package org.devops
 
+// 封装HTTP
+def HttpReqByHttpRequest(reqType, reqUrl,reqBody ) {
+    def gitServer = "http://172.31.0.10/api/v4"
+    withCredentials([string(credentialsId: 'a6679475-5c2c-49f0-8ac9-288ecdf37672', variable: 'GITLABTOKEN')]) {
+        response = httpRequest acceptType: 'APPLICATION_JSON_UTF8', 
+                          consoleLogResponseBody: true, 
+                          contentType: 'APPLICATION_JSON_UTF8', 
+                          customHeaders: [[maskValue: false, name: 'PRIVATE-TOKEN', value: "${GITLABTOKEN}"]], 
+                          httpMode: "${reqType}", 
+                          url: "${gitServer}/${reqUrl}", 
+                          wrapAsMultipart: false,
+                          requestBody: "${reqBody}"
+    }
+    return response
+}
+
+//创建文件
+def CreateRepoFile(projectId, filePath, fileContent, branchName) {
+    apiUrl = "projects/${projectId}/repository/files/${filePath}"
+    reqBody = """{"branch": "${branchName}","encoding":"base64", "content": "${fileContent}", "commit_message": "update a new file"}"""
+    response = HttpReqByHttpRequest('POST', apiUrl, reqBody)
+    println(response)
+}
+
 //获取文件内容
 def GetRepoFile(projectId, filePath, branchName) {
 	//GET /projects/:id/repository/files/:file_path/raw
