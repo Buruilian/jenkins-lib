e82a0f25e37e3e0de2a63ac17e84c2d22a2481c4
Administrator <2633018506@qq.com>
2023-01-27 19:28:32 +0000

    Update src/org/devops/Gitlab.groovy

--- src/org/devops/Gitlab.groovy
+++ src/org/devops/Gitlab.groovy
@@ -1,5 +1,13 @@
 package org.devops
 
+//获取文件内容
+def GetRepoFile(projectId, filePath, branchName) {
+	//GET /projects/:id/repository/files/:file_path/raw
+	apiUrl = "/projects/${projectId}/repository/files/${filePath}/raw?ref=${branchName}"
+	response = HttpReq('GET', apiUrl)
+	return response.content
+}
+
 //获取CommitID
 def GetCommitID(){
     ID = sh returnStdout: true, script:"git rev-parse HEAD"
@@ -37,7 +45,6 @@ def HttpReq(method, apiUrl){
         http://172.31.0.10/api/v4/${apiUrl} \
         --header 'PRIVATE-TOKEN: j53sXHNtGH3s7GGsT-CD' \
     """
-    response = readJSON text: response - "\n"
     return response
 }
 
@@ -45,5 +52,6 @@ def HttpReq(method, apiUrl){
 def GetBranchCommitID(projectID, branchName){
     apiUrl = "projects/${projectID}/repository/branches/${branchName}"
     response = HttpReq("GET", apiUrl)
+    response = readJSON text: response - "\n"
     return response.commit.short_id
 }
